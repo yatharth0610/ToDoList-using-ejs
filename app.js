2bb6ad7ab59cd6aacf0f2532e6dc3ee89a24a766
Yatharth Goswami <57530585+yatharth0610@users.noreply.github.com>
2020-03-23 23:04:00 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended : true}))
+app.use(express.static("public"));
+
+
+app.get("/", function(req, res){
+    let today = new Date();
+    let options = {
+        weekday : "long",
+        day : "numeric",
+        month : "long"
+    }
+
+    let day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", {
+        listTitle : day,
+        newListItem : items
+    })
+})
+
+app.post("/", function(req, res){
+    let item = req.body.newItem;
+    
+    if (req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else {
+        items.push(item);
+        res.redirect("/");
+    }
+
+})
+
+app.get("/work", function(req, res){
+    res.render("list", {listTitle : "Work List", newListItem : workItems});
+})
+
+app.post("/work", function(req, res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/");
+})
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+})
